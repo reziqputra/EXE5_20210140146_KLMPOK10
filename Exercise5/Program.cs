e63855969421e759ad685911e9ce6f453c42ca1c
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-12-20 00:07:43 +0800

    queue with single linked list

--- Exercise5/Program.cs
+++ Exercise5/Program.cs
@@ -1,48 +1,142 @@
-﻿namespace CircularQueuesUsingSingleLinkedList
+﻿using System;
+
+namespace Exercise
 {
-        class Node
-        {
-            private int data { get; set; }
-            public Node previous { get; set; }
-            public Node(int val)
-            {
-                data = val;
-                previous = null;
-            }
-        }
-        class Queue
-        {
-            private Node Reziq, Putera, Prev = null;
-        private int val;
+    class Node
+    {
+        public int number;
+        public string name;
+        public Node next;
+    }
+
+    class Queue
+    {
+        Node reziq, putera;
 
         public Queue()
+        {
+            reziq = null;
+            putera = null;
+        }
+
+        public void addNumb()
+        {
+            int no;
+            string nm;
+            Console.Write("Enter a number: ");
+            no = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a name: ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.number = no;
+            newnode.name = nm;
+            if (reziq == null || (no <= reziq.number))
             {
-                Reziq = Putera = null;
+                if ((reziq != null) && (no == reziq.number))
+                {
+                    Console.WriteLine("Duplicate number is not allowed");
+                    return;
+                }
+                newnode.next = reziq;
+                newnode.next = putera;
+                reziq = putera = newnode;
+                return;
             }
-            public void Insert()
+            while ((reziq != null) && (no >= putera.number))
             {
-                Node n = new Node(val);
-                if (Prev == null)
+                if (no == putera.number)
                 {
-                    Reziq = Putera = n;
+                    Console.WriteLine("Duplicate number is not allowed");
+                    return;
                 }
-                else if (Reziq == Putera)
-                {
-                    Putera.previous = n;
-                    Reziq = Putera.previous;
-                }
-                else
-                {
-                    Reziq.previous = n;
-                    Reziq = Reziq.previous;
-                }
+                putera.next = newnode;
+                putera = newnode;
+                return;
             }
         }
+        public bool listEmpty()
+        {
+            if (reziq == null)
+                return true;
+            else
+                return false;
+        }
+
+        public void delNumb()
+        {
+            Console.WriteLine("The deleted node is: " + reziq.number + " " + reziq.name);
+            reziq = reziq.next;
+        }
+
+        public void displayNumb()
+        {
+            if (listEmpty())
+            {
+                Console.WriteLine("List is empty. ");
+            }
+            else
+            {
+                Console.WriteLine("The records in the list are: ");
+                Node currNode;
+                for (currNode = reziq; currNode != null; currNode = currNode.next)
+                    Console.WriteLine(" - " + currNode.number + "." + currNode.name);
+                Console.WriteLine();
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-
+            Queue q = new Queue();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\n Menu ");
+                    Console.WriteLine("a. Insert Record");
+                    Console.WriteLine("b. Delete Record");
+                    Console.WriteLine("c. Display");
+                    Console.WriteLine("d. Exit");
+                    Console.Write("Choice a-d :");
+                    char ch = Convert.ToChar(Console.ReadLine());
+                    switch (ch)
+                    {
+                        case 'a':
+                            {
+                                q.addNumb();
+                            }
+                            break;
+                        case 'b':
+                            {
+                                if (q.listEmpty())
+                                {
+                                    Console.WriteLine("List is empty. ");
+                                    break;
+                                }
+                                q.delNumb();
+                            }
+                            break;
+                        case 'c':
+                            {
+                                q.displayNumb();
+                            }
+                            break;
+                        case 'd':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("\n Invalid Option");
+                                break;
+                            }
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("\n Check again!.");
+                }
+            }
         }
     }
+
 }
