82b767643ce45ae14a79379484d4ab4d3eea5f4a
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-12-19 17:24:36 +0800

    Add project files.

---
+++ Exercise5/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace CircularQueuesUsingSingleLinkedList
+{
+        class Node
+        {
+            private int data { get; set; }
+            public Node previous { get; set; }
+            public Node(int val)
+            {
+                data = val;
+                previous = null;
+            }
+        }
+        class Queue
+        {
+            private Node Reziq, Putera, Prev = null;
+            public Queue()
+            {
+                Reziq = Putera = null;
+            }
+            public void Insert()
+            {
+                Node n = new Node(val);
+                if (Prev == null)
+                {
+                    Reziq = Putera = n;
+                }
+                else if (Reziq == Putera)
+                {
+                    Putera.previous = n;
+                    Reziq = Putera.previous;
+                }
+                else
+                {
+                    Reziq.previous = n;
+                    Reziq = Reziq.previous;
+                }
+            }
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
